4c5eb20613e19cf1640ab74dc2821d43212b59d9
Jackson Chou <JacksonChou@users.noreply.github.com>
2018-01-22 00:09:50 -0800

    Updated solution with summarize operation

--- datecreation.R
+++ datecreation.R
@@ -4,22 +4,23 @@
 dat <- read.table(text="        ID  Date Duration
 407 2006-06-01       1
                   408 2006-06-02       1
-                  409 2006-06-03       2
-                  410 2006-06-04       3
-                  411 2006-06-05       1
-                  411 2006-06-06       2
+                  408 2006-06-03       2
+                  409 2006-06-04       1
+                  409 2006-06-05       2
+                  410 2006-06-06       1
                   413 2006-06-07       1
-                  414 2006-06-08       4
-                  415 2006-06-09       1", header=T)
+                  413 2006-06-08       2
+                  413 2006-06-09       3", header=T)
 
 library(dplyr)
 library(lubridate)
+library(anytime)
 
 dat$Date <- as.Date(dat$Date)
 
-dat2 <- dat %>%
-  filter(Duration <=3) %>% 
-  group_by(ID, Date) %>% 
-  mutate(Start_Date = Date) %>%
-  mutate(End_Date = if_else(Duration==1, Date, Date + Duration))
+dat %>%
+  group_by(ID) %>%
+  summarize(MaxDuration = max(Duration),
+            StartDate = min(Date),
+            EndDate = if_else(MaxDuration== 1, anydate(StartDate), anydate(StartDate + MaxDuration)))
 
